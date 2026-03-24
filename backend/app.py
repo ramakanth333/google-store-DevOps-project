285d2c2d0a31d88471511d5f8a112563acc35b50
Ramakanth yadav <73477565+ramakanth333@users.noreply.github.com>
2026-03-24 12:32:46 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,7 @@ CORS(app)
 
 # ---------------- DATABASE CONFIG ----------------
 db_config = {
-    "host": "database-1.cr84emu2ej6t.us-east-1.rds.amazonaws.com",
+    "host": "database-1.catkwuy8ivm3.us-east-1.rds.amazonaws.com",
     "user": "admin",
     "password": "Cloud123",
     "database": "cloud"
